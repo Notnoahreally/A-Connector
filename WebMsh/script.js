eedc24fe75fdaa8ee82ba2afb25537e99f25a997
Notnoahreally <121029568+Notnoahreally@users.noreply.github.com>
2023-05-21 15:47:56 +0800

    Add files via upload

---
+++ WebMsh/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('myButton');
+  button.addEventListener('click', function() {
+    setTimeout(function() {
+      window.location.replace('https://www.youtube.com/channel/UCtlPrFEsL8Of2wI9Sl2EkZw');
+    }, 2000); // 1000 milliseconds = 1 second
+  });
+});
+
+
+//The Other .Html is still in development...
